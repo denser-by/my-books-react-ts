92f73b121d8495916f4038888c554e6d1ad45e8a
Dzianis Siarheyeu <denser.by@gmail.com>
2023-02-28 12:48:01 +0300

    modal menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Menu from './Menu/Menu';
+import Modal from './Modal/Modal';
 
 function App() {
+  const menuItems = [
+    {value:"Приветствие", href:'/main', icon: "android"},
+    {value:"Достижения", href:'/extension', icon: "apps"},
+    {value:"Комманда", href:'/people', icon: "assignment_ind"},
+    {value:"В последнее время", href:'/news', icon: "announcement"},
+    {value:"О нас", href:'/about', icon: "archive"}
+  ]
+
+  const [menuActive, setMenuActive] = useState(false)
+  const [modalActive, setModalActive] = useState(false)
+
+
+  function onMenuOpen() {
+    setMenuActive(!menuActive)
+  }
+
+  function openModalDialog() {
+    setModalActive(!modalActive)
+  }
+
   return (
     <div className="App">
+      <nav>
+        <div className='burger-btn' onClick={onMenuOpen}>
+          <span />
+        </div>
+      </nav>
+      <main>
+        <button className='open-btn' onClick={openModalDialog}>Открыть окно</button>
+        <p>Hello</p>
+      </main>
+
+      <Modal active={modalActive} setActive={setModalActive} >
+        <form action="">
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+          <button></button>
+        </form>
+      </Modal>
+
+
+      <Menu active={menuActive} setActive={setMenuActive} header={"Меню закладки"} items={menuItems} />
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './menu.css';
+
+const Menu = ({header, items, active, setActive}) => {
+    return (
+        <div className={active ? "menu active" : "menu"} onClick={()=>setActive(false)}>
+            <div className="blur" />
+            <div className="munuContent" onClick={(e)=>e.stopPropagation()}>
+                <div className="menuHeader">{header}</div>
+                <ul>
+                    {items.map(item=>
+                        <li>
+                            <a href={item.href}>{item.value}</a>
+                            <span className="material-icons">{item.icon}</span>
+                        </li>
+                    )}
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
