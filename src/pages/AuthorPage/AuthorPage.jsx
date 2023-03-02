e411858475c509d98a6eb056478b4882acd3956c
Dzianis Siarheyeu <denser.by@gmail.com>
2023-03-02 13:42:28 +0300

    common hidden

--- src/pages/AuthorPage/AuthorPage.jsx
+++ src/pages/AuthorPage/AuthorPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './authorpage.css'
-import {Form, Input, Button} from 'reactstrap';
+import './../common.css'
+import { Form, Input, Button } from 'reactstrap';
 
 const AuthorPage = ({ author, edit }) => {
     return (
@@ -12,27 +13,28 @@ const AuthorPage = ({ author, edit }) => {
                 <span className="icons-right">
                     <span className="author-info">
                         <span className="author-info-label">Author name:</span>
-                        {/* <textarea id="authorName" name="authorName" readOnly={!edit} /> */}
-
-                        <Input id="authorName" name="authorName" type="search" readOnly={!edit} className="mr-3" placeholder="Author name" />
-
+                        <textarea id="authorName" name="authorName" readOnly={!edit} placeholder="Author name"
+                            className={!edit ? "ctrlHidden" : "fieldCurrent"} />
                         <span className="fieldCurrent">{author.name}</span>
                     </span>
                     <span className="author-info">
                         <span className="author-info-label">Born in:</span>
-                        <textarea id="authorAge" name="authorAge" readOnly={!edit} />
+                        <textarea id="authorAge" name="authorAge" readOnly={!edit} placeholder="born in"
+                            className={!edit ? "ctrlHidden" : "fieldCurrent"} />
                         <span className="fieldCurrent">{author.age}</span>
                     </span>
                     <span className="author-info">
                         <span className="author-info-label">List of books:</span>
-                        <textarea id="authorBooks" name="authorBooks" readOnly={!edit} />
+                        <textarea id="authorBooks" name="authorBooks" readOnly={!edit} placeholder="published books list"
+                            className={!edit ? "ctrlHidden" : "fieldCurrent"} />
                         <span className="fieldCurrent">{author.books}</span>
                     </span>
                 </span>
             </span>
             <div className="author-info">
                 <span className="author-info-label">Biography:</span>
-                <textarea id="authorInfo" name="authorInfo" readOnly={!edit} />
+                <textarea id="authorInfo" name="authorInfo" readOnly={!edit} placeholder="Short books related description"
+                    className={!edit ? "ctrlHidden" : "fieldCurrent"} />
                 <div className="fieldCurrent">{author.info}</div>
             </div>
             <input type={edit ? "submit" : "hidden"} value="Save" />
--- src/pages/BookPage/BookPage.jsx
+++ src/pages/BookPage/BookPage.jsx
@@ -1,65 +1,52 @@
 import React, { useState } from 'react';
 import './bookpage.css'
+import './../common.css'
 import { Form, Input, Button } from 'reactstrap';
-
-
 
 const BookPage = ({ book, edit }) => {
 
-//    const [publishYear, setPublishYear] = useState(book.year)
     const [state, setState] = useState({
         name: book.name,
         year: book.year,
-        info:"3"
+        info: "3"
     })
-
-    function handleYearChange(event) {
-        setPublishYear(event.target.value)
-    }
 
     return (
         <form>
-        <span className="bookShape">
-            <span className='bookShapeHeader'>
-                <span className="picture">
-                    <img src={book.cover_img} />
-                </span>
-                <span className="icons-right">
-                    <span className="book-info">
-                        <span className="book-info-label">Book title:</span>
-
-                        {/* <Input id="bookName" name="bookName" type="search" readOnly={!edit} className="mr-3" placeholder="Book name with short description" /> */}
-                        <input type="text" value={state.name} readOnly={!edit} />
-
-                        <textarea id="bookName" name="bookName" readOnly={!edit} />
-                        <span className="fieldCurrent">{book.name}</span>
+            <span className="bookShape">
+                <span className='bookShapeHeader'>
+                    <span className="picture">
+                        <img src={book.cover_img} />
                     </span>
-                    <span className="book-info">
-                        <span className="book-info-label">Year of publication:</span>
-
-                        <input type="text" value={state.year} readOnly={!edit} />
-
-                        <textarea id="bookYear" name="bookYear" readOnly={!edit} />
-                        <span className="fieldCurrent">{book.year}</span>
-
-                    </span>
-                    <span className="book-info">
-                        <span className="book-info-label">Authors:</span>
-                        <textarea id="bookAuthors" name="bookAuthors" readOnly={!edit} />
-                        <span className="fieldCurrent">{book.authors}</span>
+                    <span className="icons-right">
+                        <span className="book-info">
+                            <span className="book-info-label">Book title:</span>
+                            <textarea id="bookName" name="bookName" readOnly={!edit} placeholder="Book name with short description"
+                                className={!edit ? "ctrlHidden" : "fieldCurrent"} />
+                            <span className="fieldCurrent">{book.name}</span>
+                        </span>
+                        <span className="book-info">
+                            <span className="book-info-label">Year of publication:</span>
+                            <textarea id="bookYear" name="bookYear" readOnly={!edit} placeholder="Year of publication"
+                                className={!edit ? "ctrlHidden" : "fieldCurrent"} />
+                            <span className="fieldCurrent">{book.year}</span>
+                        </span>
+                        <span className="book-info">
+                            <span className="book-info-label">Authors:</span>
+                            <textarea id="bookAuthors" name="bookAuthors" readOnly={!edit} placeholder="Authorship entry"
+                                className={!edit ? "ctrlHidden" : "fieldCurrent"} />
+                            <span className="fieldCurrent">{book.authors}</span>
+                        </span>
                     </span>
                 </span>
+                <div className="book-info">
+                    <span className="book-info-label">Book description:</span>
+                    <textarea id="bookInfo" name="bookInfo" readOnly={!edit} placeholder="Remarkable book notes"
+                        className={!edit ? "ctrlHidden" : "fieldCurrent"} />
+                    <div className="fieldCurrent">{book.info}</div>
+                </div>
+                <input type={edit ? "submit" : "hidden"} value="Save" />
             </span>
-            <div className="book-info">
-                <span className="book-info-label">Book description:</span>
-
-                <input type="text" value={state.info} readOnly={!edit} />
-
-                <textarea id="bookInfo" name="bookInfo" readOnly={!edit} />
-                <div className="fieldCurrent">{book.info}</div>
-            </div>
-            <input type={edit ? "submit" : "hidden"} value="Save" />
-        </span>
         </form>
     );
 };
