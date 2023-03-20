77adf44718b2cec707fa5726ffdb5b45be9d24e1
Dzianis Siarheyeu <denser.by@gmail.com>
2023-03-20 11:57:47 +0300

    multiselect fine

--- src/components/TextList/TextListCompon.js
+++ src/components/TextList/TextListCompon.js
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import './TextListCompon.css'
 
+
+export function TextListEditItem({ item, selectionsList, setSelectionsList }) {
+
+    const [stateActive, setStateActive] = React.useState(false);
+
+    const onEditActivate = (e) => {
+        let current = "" + e.item;
+        let newArr = [];
+        if (stateActive) {
+            selectionsList.map(item => {
+                if (item != current)
+                    newArr.push(item);
+            });
+        } else {
+            newArr.push(current);
+            newArr.push(...selectionsList);
+        }
+        setSelectionsList(newArr);
+        setStateActive(!stateActive);
+    };
+
+    return (
+        ("" + item).length > 0
+            ?
+            <span className={"textListEditItem" + (stateActive ? " selected" : "")} onClick={(e) => onEditActivate({ item })}>{item}</span>
+            :
+            <span />
+    );
+};
+
 export function TextListEdit({ text, className }) {
 
     var listItems = [].concat(text.split("\n"));
+
+    const [selectionsList, setSelectionsList] = React.useState([]);
 
     return (
         <span className={className + ' mainWidth'}>
@@ -11,11 +43,7 @@ export function TextListEdit({ text, className }) {
                 {
                     listItems.map(item => {
                         return (
-                            ("" + item).length > 0
-                                ?
-                                <span className='textListEditItem'>{item}</span>
-                                :
-                                <span />
+                            <TextListEditItem item={item} selectionsList={selectionsList} setSelectionsList={setSelectionsList} />
                         );
                     })
                 }
